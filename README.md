8269d468da275d076e9f99a1566dc509d5fb78c9
mayuc97 <mayrauraga@gmail.com>
2021-08-25 16:51:09 -0500

    Añadí archivo de Arduino y modifique READ ME

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # web-serial
 Test of the web server communicated by serial port
+
+Descripción: Comunicacion serial de prueba
---
+++ pruebaArduino/pruebaArduino.ino
@@ -0,0 +1,30 @@
+int input;
+
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  //Para conocer si existen datos pendientes en 
+  //el bus de comunicación serial
+  if (Serial.available()>0){
+
+    input= Serial.read(); //Lee datos desde el puerto serial
+
+    if (input=='1'){
+        digitalWrite(2,HIGH); //Si mandamos un 1 a través de la consola
+        //el LED se enciende
+        delay(1000);
+      }
+    else
+      {
+        digitalWrite(2,LOW); //Si mandamos un valor diferente el LED
+        //se apaga
+      }
+      
+    Serial.println(input); //Muestra el dato con salto de línea
+    }
+
+}
