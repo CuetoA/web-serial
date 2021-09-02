a00367fe74f6d8fa05b9db1c6dc876adec0db476
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-09-01 22:08:24 -0500

    added python file to set server authomatically

---
+++ set_server.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  1 21:46:27 2021
+
+@author: Cuetorra
+Server
+"""
+import os
+import subprocess
+
+cur_folder = os.path.dirname(os.path.abspath(__file__))
+
+
+p_file = 'C:\Windows\System32\powershell.exe '
+cmd1 = 'cd '
+cmd2 = 'nodemon servidor.js'
+
+
+command_1 = p_file + cmd1 + cur_folder
+command_2 = p_file + cmd2 
+command_3 = p_file + r'C:\Users\ADMIN\Desktop\start.JPG'
+
+print(command_1)
+print(command_2)
+
+#subprocess.call(command_1, shell=True)
+#subprocess.call(command_2, shell=True)
+subprocess.call(command_3, shell=True)
