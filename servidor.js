3704a40ca8d9a821636303bc0184932b5930139e
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-09-15 16:43:23 -0500

    Reordered servidor.js script

--- servidor.js
+++ servidor.js
@@ -6,17 +6,17 @@ const app = express();
 const server = http.createServer(app);
 var io = require('socket.io')(server);
 
+const Serialport = require("serialport");
+const Readline = Serialport.parsers.Readline;
+const port = new Serialport('COM6',{baudRate: 115200, databits: 8, parity: 'none', stopbits: 1, flowControl: false, buffersize: 32768});
+const parser = port.pipe(new Readline());
+
+app.get('/', function(req, res){ res.sendFile(__dirname + 'index.html') });
 app.use(express.static(__dirname + '/')); // Main path
 server.listen(8080,() => { 
 	console.log('Server lsitening on http://localhost:8080') 
 });
 
-
-
-const Serialport = require("serialport");
-const Readline = Serialport.parsers.Readline;
-const port = new Serialport('COM6',{baudRate: 115200, databits: 8, parity: 'none', stopbits: 1, flowControl: false, buffersize: 32768});
-const parser = port.pipe(new Readline());
 
 parser.on('open', function() {
 	console.log('connection is opened');
@@ -30,6 +30,3 @@ parser.on('data', function(data){
 port.on('error', function(err){
 	console.log(err);
 });
-
-
-app.get('/', function(req, res){ res.sendFile(__dirname + 'index.html') });
