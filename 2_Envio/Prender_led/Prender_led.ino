9a15bdb508f3004df6555445ee0f8d22648de5b5
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-10-07 17:58:43 -0500

    Achived Led turns on and of with serial

--- 2_Envio/Prender_led/Prender_led.ino
+++ 2_Envio/Prender_led/Prender_led.ino
@@ -1,30 +1,31 @@
-int incomingByte = 0; // for incoming serial data
-int valor;
-#define LED_VERDE 12
-#define LED_ROJO 10
+int valor = 0; // for incoming serial data
+#define LED 12
 
 
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
-  pinMode(LED_VERDE, OUTPUT);
-  pinMode(LED_ROJO, OUTPUT);
-  
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // send data only when you receive data:
-
-  valor = Serial.read();
-  digitalWrite(LED_VERDE, HIGH);
-  digitalWrite(LED_ROJO, HIGH);
-  
   
   if (Serial.available() > 0) {
     // read the incoming byte:
-    incomingByte = Serial.read();
-
+    valor = Serial.read();
     // say what you got:
-    Serial.print("I received: ");
-    Serial.println((char)incomingByte);
+    Serial.print("Recibí: ");
+    Serial.println((char)valor);
+
+    if (valor == '1'){
+      digitalWrite(LED, HIGH);
+    }
+    else if (valor == '0'){
+      digitalWrite(LED, LOW);
+    }
+    else{
+      Serial.println("Valor no entendido");
+    }
+    delay(200);
   }
 }
