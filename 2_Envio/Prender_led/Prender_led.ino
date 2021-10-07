04a35a7e3b25c24744973354352ccc9e278886ee
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-10-07 17:34:19 -0500

    testing arduino program

--- 2_Envio/Prender_led/Prender_led.ino
+++ 2_Envio/Prender_led/Prender_led.ino
@@ -1,11 +1,24 @@
 int incomingByte = 0; // for incoming serial data
+int valor;
+#define LED_VERDE 12
+#define LED_ROJO 10
+
 
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(LED_ROJO, OUTPUT);
+  
 }
 
 void loop() {
   // send data only when you receive data:
+
+  valor = Serial.read();
+  digitalWrite(LED_VERDE, HIGH);
+  digitalWrite(LED_ROJO, HIGH);
+  
+  
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
