b6833bca1fdfcf5aa4acdc7b0ff44a96c6bc2218
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-10-15 17:37:44 -0500

    cleaning e-index.js

--- 2_Envio/e-index.js
+++ 2_Envio/e-index.js
@@ -1,8 +1,16 @@
-// const socket = io();
 const socket = io("http://localhost:8080");
-// Constantes para prender led
 const prender = document.getElementById("on-button")
 const apagar = document.getElementById("off-button")
+
+
+// Función de envío
+function enviarDatos(valor) {
+	if (valor === "1") {
+		socket.emit('encender', valor);
+	} else {
+		socket.emit('apagar', valor);
+	}
+}
 
 
 // Comportamiento de botones
@@ -19,21 +27,3 @@ socket.on("an_event", (data) => {
 	socket.emit("hi_back", "hola de vuelta");
 });
 
-// Función de envío
-function enviarDatos(valor) {
-	//console.log('prueba prueba');
-	//console.log('se envió un: ', valor);
-	//console.log('');
-	//console.log(socket)
-	// Si es un 1
-	if (valor === "1") {
-		// emitir un evento de encendido
-		socket.emit('encender', valor);
-		console.log('Test encender 1');
-		// Si es un  0
-	} else {
-		// emitir un evento de apagado
-		socket.emit('apagar', valor);
-		console.log('Test apagar 1');
-	}
-}
