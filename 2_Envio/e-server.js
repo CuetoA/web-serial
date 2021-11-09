39896d0da5947fdfc207efec91980bf7a38a1a13
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-11-08 21:14:05 -0600

    Added a port listener

--- 2_Envio/e-server.js
+++ 2_Envio/e-server.js
@@ -40,6 +40,13 @@ io.on("connection", (socket) => {
 	});
 });
 
+
+// Emite evento temp cuando escucha evento data
+parser.on('data', function(data){
+	console.log('Se está recibiendo: ',data);
+});
+
+
 //Llamado a la página
 app.get('/', function (req, res) { res.sendFile(__dirname + '/e-index.html') });
 
